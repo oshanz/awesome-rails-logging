c3d742442871206c64d8f7fc3ce6560db8bc8600
Oshanz <spiritualmirror@gmail.com>
2016-09-04 14:23:36 +0530

    comment logstash gem because of issue https://github.com/cmertz/logstash_rails/issues/6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  LogstashRails.config(transport: :logfile)
+  #LogstashRails.config(transport: :logfile)
 end
